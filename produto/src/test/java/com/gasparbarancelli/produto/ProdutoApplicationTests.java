90f753f017f1d8ecfdc10ecb755fffe7643f00e3
Marcio Cardozo <marcio.cardozo@softfocus.com.br>
2020-11-28 11:11:45 -0300

    Softfocus

---
+++ produto/src/test/java/com/gasparbarancelli/produto/ProdutoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gasparbarancelli.produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+class ProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
